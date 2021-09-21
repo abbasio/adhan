323e9db171f3a9f75aef033dfd4bc8f9333d59d9
abbasio <omarabbasi16@gmail.com>
2021-09-21 18:19:06 -0400

    Added location services

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
+//---------GET DATE
 const today = new Date();
 const date = today.getDate() - 1;
 const month = today.getMonth() + 1;
 const dateText = document.querySelector('#date');
 const timingsText = document.querySelector('#timings');
 
-function ifLocation(position){
-    const coordinates = position.coords;
-    console.log(coordinates.latitude);
-    console.log(coordinates.longitude);
-}
-function noLocation(error){
-    console.log(`ERROR(${error.code}): ${error.message}`);
-}
-navigator.geolocation.getCurrentPosition(ifLocation, noLocation);
 
-async function getTimes(){
-    const apiURL = `http://api.aladhan.com/v1/calendarByCity?city=New York&country=United States&method=2&month=${month}&year=2021`;
-    const response = await fetch(apiURL, {mode: 'cors'});
-    const timeData = await response.json();
-    const timeObject = timeData.data[date].timings;
-    dateText.textContent = timeData.data[date].date.readable;
-    for (let key in timeObject){
-        const newDiv = document.createElement('div');
-        newDiv.id = "time"
-        newDiv.textContent = `${key} : ${(timeObject[key]).replace('(EDT)', '')}`
-        timingsText.appendChild(newDiv);
-        console.log(key)
-        console.log(timeObject[key])
+navigator.geolocation.getCurrentPosition((position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    async function getTimes() {
+        const apiURL = `http://api.aladhan.com/v1/calendar?latitude=${latitude}&longitude=${longitude}&method=2&month${month}&year=2021`;
+        const response = await fetch(apiURL, { mode: 'cors' });
+        const timeData = await response.json();
+        const timeObject = timeData.data[date].timings;
+        dateText.textContent = timeData.data[date].date.readable;
+        for (let key in timeObject) {
+            const newDiv = document.createElement('div');
+            newDiv.id = "time"
+            newDiv.textContent = `${key} : ${(timeObject[key]).replace(/\s*\(.*?\)\s*/g, '')}`
+            timingsText.appendChild(newDiv);
+            console.log(key)
+            console.log(timeObject[key])
+        }
+
     }
+    getTimes();
+});
 
-}
 
-getTimes();
+
