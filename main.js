3a44f7d19f0d91f6ed710943d993d553eb4fbf4e
abbasio <omarabbasi16@gmail.com>
2021-09-21 12:49:56 -0400

    Added manifest, popup, and CSS

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+const today = new Date();
+const date = today.getDate() - 1;
+const month = today.getMonth() + 1;
+const dateText = document.querySelector('#date');
+const timingsText = document.querySelector('#timings');
+
 
 async function getTimes(){
-    const today = new Date();
-    let date = today.getDate() - 1;
-    const apiURL = 'http://api.aladhan.com/v1/calendarByCity?city=New York&country=United States&method=2&month=09&year=2021';
+    const apiURL = `http://api.aladhan.com/v1/calendarByCity?city=New York&country=United States&method=2&month=${month}&year=2021`;
     const response = await fetch(apiURL, {mode: 'cors'});
     const timeData = await response.json();
-    console.log(timeData.data[date].timings);
-    console.log(date);
+    timingsText.textContent = JSON.stringify(timeData.data[date].timings);
+    dateText.textContent = timeData.data[date].date.readable;
 }
 
 getTimes();
