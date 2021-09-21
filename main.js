23504ed4affb9f33f4b331496a656d147b5d9890
abbasio <omarabbasi16@gmail.com>
2021-09-21 16:58:19 -0400

    Added geolocation API

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ const month = today.getMonth() + 1;
 const dateText = document.querySelector('#date');
 const timingsText = document.querySelector('#timings');
 
+function ifLocation(position){
+    const coordinates = position.coords;
+    console.log(coordinates.latitude);
+    console.log(coordinates.longitude);
+}
+function noLocation(error){
+    console.log(`ERROR(${error.code}): ${error.message}`);
+}
+navigator.geolocation.getCurrentPosition(ifLocation, noLocation);
+
 async function getTimes(){
     const apiURL = `http://api.aladhan.com/v1/calendarByCity?city=New York&country=United States&method=2&month=${month}&year=2021`;
     const response = await fetch(apiURL, {mode: 'cors'});
@@ -18,6 +28,7 @@ async function getTimes(){
         console.log(key)
         console.log(timeObject[key])
     }
+
 }
 
 getTimes();
